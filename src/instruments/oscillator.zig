3bed9e1512123d39349499e5838ed0ac089c4453
xenone <rosenkranz1999@gmail.com>
2021-07-01 18:52:42 -0400

    making synth use oscilator objects instead of functions

--- src/instruments/oscillator.zig
+++ src/instruments/oscillator.zig
@@ -25,28 +25,43 @@ fn w(hertz: f64) callconv(.Inline) f64 {
     return 2.0 * math.pi * hertz;
 }
 
-pub fn osc(t: f64, hertz: f64, oscType: OscType, LFO: struct {hertz: f64 = 0.0, amp: f64 = 0.0}) callconv(.Inline)  f64 {
-    const freq = w(hertz) * t + LFO.amp * hertz * @sin(w(LFO.hertz) * t);
-    return switch (oscType) {
-        .sin => @sin(freq),
-        .sqr => {
-            if (@sin(freq) > 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        },
-        .tri => math.asin(@sin(freq)) * 2.0 / math.pi,
-        .dsaw => (2.0 / math.pi) * (hertz * math.pi * @mod(t, 1.0/hertz) - (2.0 / math.pi)),
-        .asaw => {
-            // TODO: fix this
-            var output: f64 = 0.0;
-            var n: f64 = 0;
-            while(n < 40) : (n+=1) {
-                output += (@sin(n * freq)) / n;
-            }
-            return output * (2.0 / math.pi);
-        },
-        .noise => r.random.float(f64),
-    };
-}
+/// An oscillator (duh)
+pub const Oscillator = struct {
+    /// The type of oscilator this is
+    osc_type: OscType,
+    // what octave is this targeting relative to the given one
+    octave: i8 = 0,
+    /// how much influence this osc has on the overal sound
+    amplitude: f64 = 1.0,
+    //TODO another oscilator to be an LFO?
+    
+    const Self = @This();
+
+    pub fn val(self: Self, t: f64, hertz: f64) callconv(.Inline)  f64 {
+        //const freq = w(hertz) * t + LFO.amp * hertz * @sin(w(LFO.hertz) * t);
+        const freq = w(hertz) * t;
+        //return self.amplitude * switch (self.osc_type) {
+        return switch (self.osc_type) {
+            .sin => @sin(freq),
+            .sqr => {
+                if (@sin(freq) > 0) {
+                    return 1 * self.amplitude;
+                } else {
+                    return 0 * self.amplitude;
+                }
+            },
+            .tri => math.asin(@sin(freq)) * 2.0 / math.pi,
+            .dsaw => (2.0 / math.pi) * (hertz * math.pi * @mod(t, 1.0/hertz) - (2.0 / math.pi)),
+            .asaw => {
+                // TODO: fix this
+                var output: f64 = 0.0;
+                var n: f64 = 0;
+                while(n < 40) : (n+=1) {
+                    output += (@sin(n * freq)) / n;
+                }
+                return output * (2.0 / math.pi) * self.amplitude;
+            },
+            .noise => r.random.float(f64),
+        };
+    }
+};
--- src/instruments/synth.zig
+++ src/instruments/synth.zig
@@ -5,13 +5,34 @@ const notes = @import("notes.zig");
 const osc = @import("oscillator.zig");
 
 pub const Synth = struct {
+    /// The volume of this synth
     volume: f32 = 1.0,
+    /// Envelope that controls the way the amplitude of this synth
     env: envelope.ASDR,
-    soundFn: fn (t: f64, env: envelope.ASDR, n: *notes.Note) Frame,
+    /// Oscilators that this synth uses to generate sound
+    /// TODO: should this be a slice or array?
+    oscilators: [3]osc.Oscillator,
 
     const Self = @This();
+
+    /// Return the amplitude of this synth for the given note at the given time
     pub fn sound(self: Self, t: f64, n: *notes.Note) Frame {
-        return self.soundFn(t, self.env, n).times(self.volume);
+        // build the sound
+        // start with nothin
+        var val: f64 = 0.0;
+
+        // use all the oscillators that this synth has
+        for (self.oscilators) |o| {
+            val += o.amplitude * o.val(t, notes.freqFromScale(.{.id=n.id, .octave=o.octave}));
+        }
+
+        // add envelope
+        val *= self.env.getAmp(t, n);
+
+        return .{
+            .l = @floatCast(f32, val),
+            .r = @floatCast(f32, val),
+        };
     }
 };
 
@@ -22,19 +43,33 @@ pub fn Bell() Synth {
             .decay   = 1.0,
             .release = 1.0,
         },
-        .soundFn = bellSound,
+        .oscilators = [_]osc.Oscillator{
+            .{
+                .osc_type = .sin,
+            },
+            .{
+                .osc_type = .sin,
+                .amplitude = 0.5,
+                .octave = 2,
+            },
+            .{
+                .osc_type = .sin,
+                .amplitude = 0.25,
+                .octave = 3,
+            },
+        },
     };
 }
-
-fn bellSound(t: f64, env: envelope.ASDR, n: *notes.Note) Frame {
-    const val = env.getAmp(t, n) * (
-        1.0 * osc.osc(t, notes.freqFromScale(.{.id=n.id, .octave=1}), .sin, .{.hertz=5.0, .amp =0.001}) +
-        0.5 * osc.osc(t, notes.freqFromScale(.{.id=n.id, .octave=2}), .sin, .{}) +
-        0.25 * osc.osc(t,notes.freqFromScale(.{.id=n.id, .octave=3}), .sin, .{})
-    );
-
-    return .{
-        .l = @floatCast(f32, val),
-        .r = @floatCast(f32, val),
-    };
-}
+//
+//fn bellSound(t: f64, env: envelope.ASDR, n: *notes.Note) Frame {
+//    const val = env.getAmp(t, n) * (
+//        1.0 * osc.osc(t, notes.freqFromScale(.{.id=n.id, .octave=1}), .sin, .{.hertz=5.0, .amp =0.001}) +
+//        0.5 * osc.osc(t, notes.freqFromScale(.{.id=n.id, .octave=2}), .sin, .{}) +
+//        0.25 * osc.osc(t,notes.freqFromScale(.{.id=n.id, .octave=3}), .sin, .{})
+//    );
+//
+//    return .{
+//        .l = @floatCast(f32, val),
+//        .r = @floatCast(f32, val),
+//    };
+//}
