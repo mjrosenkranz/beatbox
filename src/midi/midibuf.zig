685a2314538c4debf1fde9d4b38cdca578ca76ac
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2021-08-08 22:12:00 -0400

    Added linear search insert for note events

--- src/midi/midibuf.zig
+++ src/midi/midibuf.zig
@@ -71,6 +71,56 @@ const MidiBuffer = struct {
 
         self.len += 1;
     }
+
+    /// Insert a note at a given time
+    /// If the time is greater than that of the last note then we append
+    /// otherwise O(n) insert time because I'm lazy
+    pub fn insert(self: *Self, t: EventType, i: u8, s: u32, d: u32, v: u8) !void {
+        // check if we have enough room
+        if (self.len == self.event_type.len - 1) {
+            return error.MidiBufferFull;
+        }
+
+        // if there are no notes then we can just push it
+        if (self.len == 0) {
+            return self.pushBack(t, i, s, d, v);
+        }
+
+        // if the last note occurs before this one then we can just push it
+        if (self.start[self.len] < s) {
+            return self.pushBack(t, i, s, d, v);
+        }
+
+        // otherwise we need to find the last note that starts before this one
+        // and move all the rest of the notes up
+
+        // we can assume that the list is already sorted so far
+        // TODO: binary search
+        // for now finna do a lazy loop
+        var counter: usize = 0;
+        //var last_time: u32 = self.start[0];
+        while (true) {
+            if (self.start[counter] > s)
+                break;
+            counter += 1;
+        }
+
+        var j: usize = counter+1;
+        var old_s = self.start[counter];
+        self.start[counter] = s;
+        //var old_t = self.event_type[i];
+        //var old_i = self.id[i];
+        //var old_d = self.duration[i];
+        //var old_v = self.velocity[i];
+        while(j <= self.len) : (j+=1) {
+            const new_s = self.start[j];
+            self.start[j] = old_s;
+            old_s = new_s;
+            //self.start[j+1] = ;
+        }
+
+        self.len += 1;
+    }
 };
 
 
@@ -109,4 +159,19 @@ test "push" {
 
 
     try std.testing.expectError(error.MidiBufferFull, buf.pushBack(.NoteOn, 54, 0, 8, 100));
+}
+
+test "insert" {
+
+    // create and destroy a midibuffer
+    var buf = try MidiBuffer.init(std.testing.allocator);
+    defer buf.deinit();
+    try buf.pushBack(.NoteOn, 60, 0, 8, 100);
+    try buf.pushBack(.NoteOn, 60, 10, 8, 100);
+    try buf.pushBack(.NoteOn, 60, 20, 8, 100);
+    try buf.insert(.NoteOn, 50, 5, 8, 100);
+    try expect(buf.start[0] == 0);
+    try expect(buf.start[1] == 5);
+    try expect(buf.start[2] == 10);
+    try expect(buf.start[3] == 20);
 }
